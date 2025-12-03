409e38f6524d6ad268ded4c4998434970a73f9f6
HamzaKhaliq921 <abdulkhaliq920285@gmail.com>
2025-12-03 13:28:53 -0700

    Scoket Programming Coffee Chatbot Client File

---
+++ CoffeeClient/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace CoffeeClient
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1 (Chat Display)
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(460, 300);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2 (Message Input)
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 325);
+            this.textBox2.Size = new System.Drawing.Size(350, 23);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1 (Send Button)
+            // 
+            this.button1.Location = new System.Drawing.Point(370, 325);
+            this.button1.Size = new System.Drawing.Size(100, 25);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(484, 361);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Coffee Client Chat";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ CoffeeClient/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace CoffeeClient
+{
+    public partial class Form1 : Form
+    {
+        TcpClient client;
+        NetworkStream ns;
+        StreamReader sr;
+        StreamWriter sw;
+        Thread listenThread;
+
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                client = new TcpClient("127.0.0.1", 8888);
+                ns = client.GetStream();
+                sr = new StreamReader(ns);
+                sw = new StreamWriter(ns) { AutoFlush = true };
+
+                // Read server greeting and menu
+                textBox1.Text = sr.ReadLine();
+                textBox1.Text += "\r\n" + sr.ReadLine();
+                textBox1.Text += "\r\n" + sr.ReadLine();
+                textBox1.Text += "\r\n" + sr.ReadLine();
+                textBox1.Text += "\r\n" + sr.ReadLine();
+
+                listenThread = new Thread(ListenForMessages);
+                listenThread.IsBackground = true;
+                listenThread.Start();
+            }
+            catch
+            {
+                MessageBox.Show("Cannot connect to server.");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text != "")
+            {
+                sw.WriteLine(textBox2.Text);
+                textBox1.Text += "\r\nClient >> " + textBox2.Text;
+                textBox2.Clear();
+            }
+        }
+
+        private void ListenForMessages()
+        {
+            try
+            {
+                while (true)
+                {
+                    string msg = sr.ReadLine();
+                    if (msg == null) break;
+
+                    Invoke(new Action(() =>
+                    {
+                        textBox1.Text += "\r\n" + msg;
+                    }));
+                }
+            }
+            catch { }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            listenThread.Abort();
+            sr.Close();
+            sw.Close();
+            ns.Close();
+            client.Close();
+        }
+    }
+}
